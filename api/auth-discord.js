99999e4c5f8cb18ff2245f1ea1210cfb37f523c6
TitouanFcr <foucart.titouan@gmail.com>
2026-04-11 12:08:13 +0200

    Delete api/auth-discord.js

--- api/auth-discord.js
+++
@@ -1,7 +0,0 @@
-export default function handler(req, res) {
-  const CLIENT_ID = process.env.DISCORD_CLIENT_ID;
-  const REDIRECT_URI = "https://gtarpdle.fr/auth/discord/callback";
-  const scope = "identify guilds.join";
-  const url = `https://discord.com/api/oauth2/authorize?client_id=${CLIENT_ID}&redirect_uri=${encodeURIComponent(REDIRECT_URI)}&response_type=code&scope=${encodeURIComponent(scope)}`;
-  res.redirect(302, url);
-}
